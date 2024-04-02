888e9e1ebe6e5f21d90bff60967b666fad2be59d
betolix <betolix@gmail.com>
2024-04-02 10:33:45 -0400

    Initial Commit BlixSalesApplication

---
+++ src/main/java/io/h3llo/blixsales/BlixSalesApplication.java
@@ -0,0 +1,15 @@
+package io.h3llo.blixsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlixSalesApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BlixSalesApplication.class, args);
+
+    }
+
+}
