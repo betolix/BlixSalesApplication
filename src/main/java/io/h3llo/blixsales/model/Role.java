c982ce871b2ca3a0a2ff26b3e986c93add566c2f
betolix <betolix@gmail.com>
2024-04-09 09:59:21 -0400

    Implementación Clase/Tabla Role

--- src/main/java/io/h3llo/blixsales/model/Role.java
+++ src/main/java/io/h3llo/blixsales/model/Role.java
@@ -1,2 +1,34 @@
-package io.h3llo.blixsales.model;public class Role {
+package io.h3llo.blixsales.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Role {
+
+
+    @Id
+    @EqualsAndHashCode.Include
+    private Integer idRole;
+
+    @Column(length = 10, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private boolean enabled = false;
+
+    /*
+    @Column(columnDefinition = "decimal(6,2)")
+    private double dinero;
+    */
+
 }
