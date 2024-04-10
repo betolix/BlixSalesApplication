9cecfbeac43c45fc4b3ae5c6636aca12ba2a637f
betolix <betolix@gmail.com>
2024-04-10 15:49:41 -0400

    Implementación Clase/Tabla Sale

---
+++ src/main/java/io/h3llo/blixsales/model/Sale.java
@@ -0,0 +1,45 @@
+package io.h3llo.blixsales.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idSale;
+
+    @ManyToOne
+    @JoinColumn(name = "id_client", nullable = false, foreignKey = @ForeignKey(name = "FK_SALE_CLIENT"))
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user", nullable = false, foreignKey = @ForeignKey(name = "FK_SALE_USER"))
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+    @Column(columnDefinition = "decimal(6,2)", nullable = false)
+    private double total;
+
+    @Column(columnDefinition = "decimal(6,2)", nullable = false)
+    private double tax;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+
+
+
+}
