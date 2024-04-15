652ac30b1e698e5a7cad22fb551d75b80ea09249
betolix <betolix@gmail.com>
2024-04-15 17:26:35 -0400

    Creacion de ClientRepo.java

--- src/main/java/io/h3llo/blixsales/repo/IClientRepo.java
+++ src/main/java/io/h3llo/blixsales/repo/IClientRepo.java
@@ -1,9 +1,9 @@
 package io.h3llo.blixsales.repo;
 
-import io.h3llo.blixsales.model.Category;
+import io.h3llo.blixsales.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICategoryRepo extends JpaRepository<Category, Integer> {
-    //public Category save(Category category);
+public interface IClientRepo extends JpaRepository<Client, Integer> {
+    //public Client save(Client category);
 
 }
