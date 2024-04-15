20cd969a2b4ef1c5d9abb8ed615ea815ea7d7ff9
betolix <betolix@gmail.com>
2024-04-15 17:21:41 -0400

    Creacion de IngressDetail.java

--- src/main/java/io/h3llo/blixsales/model/IngressDetail.java
+++ src/main/java/io/h3llo/blixsales/model/IngressDetail.java
@@ -3,7 +3,6 @@ package io.h3llo.blixsales.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
