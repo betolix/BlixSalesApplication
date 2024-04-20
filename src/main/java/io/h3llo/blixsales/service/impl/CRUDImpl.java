1105b4937c8b90af9af62d4f4422e15c1b8ae23e
betolix <betolix@gmail.com>
2024-04-20 17:07:13 -0400

    Creación de la clase exception.ModelNotFoundException - Uso de JAVA REFLECTIONS para el método update

--- src/main/java/io/h3llo/blixsales/service/impl/CRUDImpl.java
+++ src/main/java/io/h3llo/blixsales/service/impl/CRUDImpl.java
@@ -1,8 +1,10 @@
 package io.h3llo.blixsales.service.impl;
 
+import io.h3llo.blixsales.exception.ModelNotFoundException;
 import io.h3llo.blixsales.repo.IGenericRepo;
 import io.h3llo.blixsales.service.ICRUD;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 public abstract class CRUDImpl<T, ID> implements ICRUD<T, ID>{
@@ -17,6 +19,16 @@ public abstract class CRUDImpl<T, ID> implements ICRUD<T, ID>{
 
     @Override
     public T update(T t, ID id) throws Exception {
+
+        // JAVA REFLECTIONS
+        Class<?> clazz = t.getClass();
+        String className = clazz.getSimpleName();
+        String methodName = "setId" + className;
+
+        Method setIdMethod = clazz.getMethod (methodName,id.getClass());
+        setIdMethod.invoke(t, id);
+
+        getRepo().findById(id).orElseThrow( () -> new ModelNotFoundException("ID NOT FOUND: " + id) );
         return getRepo().save(t);
     }
 
@@ -27,11 +39,12 @@ public abstract class CRUDImpl<T, ID> implements ICRUD<T, ID>{
 
     @Override
     public T readById(ID id) throws Exception {
-        return getRepo().findById(id).orElse(null);
+        return getRepo().findById(id).orElseThrow( () -> new ModelNotFoundException("ID NOT FOUND: " + id) );
     }
 
     @Override
     public void delete(ID id) throws Exception {
+        getRepo().findById(id).orElseThrow( () -> new ModelNotFoundException("ID NOT FOUND: " + id) );
         getRepo().deleteById(id);
 
     }
