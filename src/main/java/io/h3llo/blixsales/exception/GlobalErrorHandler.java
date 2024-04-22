6ca954d36316f92aed055195d11db82e3be4ea48
betolix <betolix@gmail.com>
2024-04-22 16:38:49 -0400

    Uso de ProblemDetail

--- src/main/java/io/h3llo/blixsales/exception/GlobalErrorHandler.java
+++ src/main/java/io/h3llo/blixsales/exception/GlobalErrorHandler.java
@@ -2,12 +2,14 @@ package io.h3llo.blixsales.exception;
 
 import io.h3llo.blixsales.dto.GenericResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,6 +26,7 @@ public class GlobalErrorHandler {
 
     }
 
+    /*
     @ExceptionHandler(ModelNotFoundException.class)
     public ResponseEntity<GenericResponse<CustomErrorResponse>> handleModelNotFoundException(ModelNotFoundException ex, WebRequest req){
 
@@ -31,7 +34,20 @@ public class GlobalErrorHandler {
 
         return new ResponseEntity<>(new GenericResponse<>(404, "error", Arrays.asList(errorResponse)), HttpStatus.NOT_FOUND);
 
+    } */
+
+    //Desde Spring Boot 3+
+    @ExceptionHandler(ModelNotFoundException.class)
+    public ProblemDetail handleModelNotFoundException(ModelNotFoundException ex, WebRequest req){
+
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        pd.setTitle("Model Not Found Exception");
+        pd.setType(URI.create(req.getContextPath()));
+        pd. setProperty ("code", 404);
+        pd. setProperty ("message", "not-found");
+        return pd;
     }
+
 
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
